0f66bb7706380fc9be4ba51c80938fbc89d17c95
markoclimako <mkclimako@gmail.com>
2022-11-09 10:58:36 -0300

    feat(routes): create a route for the 'get' request method at the '/users' path.
     -this route will accept pagination query parameters, 'page' and 'limit', and will respond with a string that includes both values.

--- src/server.js
+++ src/server.js
@@ -2,9 +2,15 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request,response) => {
-  response.send("Hello, world!")
+app.get("/message/:id/:user", (request, response) => {
+  const { id, user } = request.params;
+  response.send(`Message id: ${id}. For user ${user}`);
 });
+
+app.get("/users",(request,response)=> {
+  const {page,limit} = request.query
+  response.send(`Page: ${page}. Limit: ${limit}`)
+})
 
 const port = 3333;
 
